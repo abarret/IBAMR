3084b3d8237c887a0f7ed1a468fe14ddb518877d
Aaron Barrett <abarret@live.unc.edu>
2018-12-11 17:48:44 -0500

    Updated interpolation to use dimension-by-dimensions quadratic fit.

--- examples/complex_fluids/ex1/InterpolationUtilities.cpp
+++ examples/complex_fluids/ex1/InterpolationUtilities.cpp
@@ -1,11 +1,12 @@
 #include "InterpolationUtilities.h"
+#include "tbox/MathUtilities.h"
 #include "tbox/Pointer.h"
+#include <Eigen/Core>
+#include <Eigen/QR>
 #include <HierarchyCellDataOpsReal.h>
 #include <Patch.h>
 #include <PatchLevel.h>
-#include <Eigen/Core>
-#include <Eigen/QR>
-#include <boost/concept_check.hpp>
+#include <algorithm>
 
 using Eigen::Matrix3d;
 using Eigen::Vector3d;
@@ -13,13 +14,103 @@ using Eigen::Vector3d;
 namespace IBTK
 {
 double
-InterpolationUtilities::interpolate(const std::vector<double>& X,
+InterpolationUtilities::interpolate(const vector<double>& X,
                                     const int data_idx,
                                     Pointer<CellVariable<NDIM, double> > Q_var,
-                                    SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > patch_hierarchy,
-                                    const std::vector<SAMRAI::solv::RobinBcCoefStrategy<NDIM>*>& bc_coefs,
+                                    Pointer<PatchHierarchy<NDIM> > patch_hierarchy,
+                                    const std::vector<RobinBcCoefStrategy<NDIM>*>& bc_coefs,
                                     const double data_time,
                                     const int depth)
+{
+    double q_val = 0.0;
+    VariableDatabase<NDIM>* var_db = VariableDatabase<NDIM>::getDatabase();
+    const int data_idx_temp =
+        var_db->registerVariableAndContext(Q_var, var_db->getContext("Interpolation"), IntVector<NDIM>(3));
+    for (int ln = 0; ln <= patch_hierarchy->getFinestLevelNumber(); ++ln)
+    {
+        Pointer<PatchLevel<NDIM> > level = patch_hierarchy->getPatchLevel(ln);
+        level->allocatePatchData(data_idx_temp);
+    }
+    HierarchyCellDataOpsReal<NDIM, double> hier_cc_data_ops(patch_hierarchy);
+    hier_cc_data_ops.copyData(data_idx_temp, data_idx);
+    typedef HierarchyGhostCellInterpolation::InterpolationTransactionComponent InterpolationTransactionComponent;
+    std::vector<InterpolationTransactionComponent> ghost_cell_components(1);
+    ghost_cell_components[0] = InterpolationTransactionComponent(
+        data_idx_temp, "CONSERVATIVE_LINEAR_REFINE", false, "CONSERVATIVE_COARSEN", "LINEAR", false, bc_coefs, NULL);
+    HierarchyGhostCellInterpolation ghost_fill_op;
+    ghost_fill_op.initializeOperatorState(ghost_cell_components, patch_hierarchy);
+    ghost_fill_op.fillData(data_time);
+    bool done = false;
+    for (int ln = patch_hierarchy->getFinestLevelNumber(); ln >= 0 && !done; --ln)
+    {
+        // Start at the finest level...
+        Pointer<PatchLevel<NDIM> > level = patch_hierarchy->getPatchLevel(ln);
+        CellIndex<NDIM> idx = IndexUtilities::getCellIndex(X, level->getGridGeometry(), level->getRatio());
+        for (PatchLevel<NDIM>::Iterator p(level); p && !done; p++)
+        {
+            Pointer<Patch<NDIM> > patch = level->getPatch(p());
+            const Pointer<CartesianPatchGeometry<NDIM> > p_geom = patch->getPatchGeometry();
+            const double* const dx = p_geom->getDx();
+            const double* const x_lower = p_geom->getXLower();
+            const Box<NDIM>& patch_box = patch->getBox();
+            Pointer<CellData<NDIM, double> > S_data = patch->getPatchData(data_idx_temp);
+            if (patch_box.contains(idx))
+            {
+                // Great. The patch is currently on this level
+                // Let's create a box that contains this data
+                Box<NDIM> box(idx, idx);
+                // Grow it by some number of grid cells
+                box.grow(IntVector<NDIM>(3));
+                // Loop through the box, make sure the point is located
+                // OUTSIDE the disk
+                std::vector<double> x(NDIM);
+                CellData<NDIM, int> i_data(box, 1, IntVector<NDIM>(0));
+                CellData<NDIM, double> si_data(box, NDIM + 1, IntVector<NDIM>(0));
+                si_data.fillAll(std::numeric_limits<double>::signaling_NaN());
+                const CellIndex<NDIM> ci_l = patch_box.lower();
+                int num = 0;
+                for (CellIterator<NDIM> i(box); i; i++)
+                {
+                    CellIndex<NDIM> ci = i();
+                    for (int d = 0; d < NDIM; ++d) x[d] = x_lower[d] + dx[d] * (ci(d) - ci_l(d) + 0.5);
+                    double r = sqrt(x[0] * x[0] + x[1] * x[1]);
+                    if (r > 1.0)
+                    {
+                        i_data(ci) = 1;
+                        si_data(ci, NDIM) = (*S_data)(ci, depth);
+                        num++;
+                    }
+                    else
+                    {
+                        i_data(ci) = -1;
+                    }
+                }
+                // We have a box containing the point and data the says if we are inside or outside disk.
+                // Find directions and interpolate. First in x, then in y.
+                std::vector<int> completed_dims;
+                q_val = InterpolationUtilities::interpolateInBoxes(
+                    idx, X, i_data, si_data, p_geom, patch_box, 0, 0, completed_dims);
+                done = true;
+            }
+        }
+    }
+    for (int ln = 0; ln <= patch_hierarchy->getFinestLevelNumber(); ++ln)
+    {
+        Pointer<PatchLevel<NDIM> > level = patch_hierarchy->getPatchLevel(ln);
+        level->deallocatePatchData(data_idx_temp);
+    }
+    q_val = SAMRAI_MPI::sumReduction(q_val);
+    return q_val;
+}
+
+double
+InterpolationUtilities::interpolateL2(const std::vector<double>& X,
+                                      const int data_idx,
+                                      Pointer<CellVariable<NDIM, double> > Q_var,
+                                      SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > patch_hierarchy,
+                                      const std::vector<SAMRAI::solv::RobinBcCoefStrategy<NDIM>*>& bc_coefs,
+                                      const double data_time,
+                                      const int depth)
 {
     double q_val = 0.0;
     VariableDatabase<NDIM>* var_db = VariableDatabase<NDIM>::getDatabase();
@@ -62,7 +153,6 @@ InterpolationUtilities::interpolate(const std::vector<double>& X,
                 box.grow(2);
                 // Loop through the box, make sure the point is located
                 // OUTSIDE the disk
-                double r;
                 std::vector<double> x(NDIM);
                 CellData<NDIM, int> i_data(box, 1, IntVector<NDIM>(0));
                 const CellIndex<NDIM> ci_l = patch_box.lower();
@@ -134,4 +224,122 @@ double InterpolationUtilities::weightFcn(const std::vector<double>& x, const std
     return w;
 }
 
+double
+InterpolationUtilities::interpolate(const double& l, const std::vector<int>& i, const std::vector<double>& y)
+{
+    double val = 0.0;
+    TBOX_ASSERT(i.size() == 3);
+    TBOX_ASSERT(y.size() == i.size());
+    val = y[0] + (y[1] - y[0]) * l / static_cast<double>(i[1] - i[0]) +
+          (y[2] * static_cast<double>(i[0] - i[1]) + y[0] * static_cast<double>(i[1] - i[2]) +
+           y[1] * static_cast<double>(i[2] - i[0])) *
+              l * (l - 1.0) / (static_cast<double>((i[0] - i[1]) * (i[0] - i[2]) * (i[1] - i[2])));
+    return val;
+}
+
+double
+InterpolationUtilities::interpolateInBoxes(const CellIndex<NDIM>& idx,
+                                           const std::vector<double>& X,
+                                           CellData<NDIM, int>& r_data,
+                                           CellData<NDIM, double>& q_data,
+                                           Pointer<CartesianPatchGeometry<NDIM> > pgeom,
+                                           const Box<NDIM>& pbox,
+                                           int dim,
+                                           int cycle,
+                                           std::vector<int>& completed_dims)
+{
+    double q_val = 0.0;
+    // form list of indices.
+    std::vector<CellIndex<NDIM> > idx_list;
+    std::vector<int> i_list;
+    bool done = false;
+    while (!done)
+    {
+        idx_list.clear();
+        i_list.clear();
+        dim = dim % NDIM;
+        if (r_data(idx) == 1)
+        {
+            idx_list.push_back(idx);
+            i_list.push_back(0);
+        }
+        int s = 1;
+        while (idx_list.size() < 3)
+        {
+            IntVector<NDIM> si(0);
+            si(dim) = s;
+            if (r_data(idx + si) == 1)
+            {
+                idx_list.push_back(idx + si);
+                i_list.push_back(s);
+            }
+            if (r_data(idx - si) == 1)
+            {
+                idx_list.push_back(idx - si);
+                i_list.push_back(-s);
+            }
+            s++;
+        }
+        int min_val = *std::min_element(i_list.begin(), i_list.end());
+        int max_val = *std::max_element(i_list.begin(), i_list.end());
+        if (std::max(std::abs(min_val), std::abs(max_val)) > 3)
+        {
+            dim++;
+            if (std::find(completed_dims.begin(), completed_dims.end(), dim) != completed_dims.end() || dim > NDIM)
+            {
+                TBOX_ERROR("already completed dimension, or dimension too high");
+            }
+        }
+        else
+        {
+            done = true;
+        }
+    }
+    while (idx_list.size() > 3)
+    {
+        idx_list.pop_back();
+    }
+    completed_dims.push_back(dim);
+    // We start at dim = 0. Each call generates a new box to interpolate inside of, and increases the dim by 1.
+    if (cycle < NDIM - 1)
+    {
+        std::vector<int> i_list;
+        std::vector<double> y_data;
+        for (std::vector<CellIndex<NDIM> >::const_iterator cit = idx_list.begin(); cit != idx_list.end(); ++cit)
+        {
+            const CellIndex<NDIM>& cidx = *cit;
+            q_data(cidx, cycle + 1) = InterpolationUtilities::interpolateInBoxes(
+                cidx, X, r_data, q_data, pgeom, pbox, dim + 1, cycle + 1, completed_dims);
+            i_list.push_back(cidx(dim));
+            y_data.push_back(q_data(cidx, cycle + 1));
+        }
+        const double* dx = pgeom->getDx();
+        const double* xlow = pgeom->getXLower();
+        const CellIndex<NDIM>& idxl = pbox.lower();
+        double xx = xlow[dim] + dx[dim] * (idx_list[0](dim) - idxl(dim) + 0.5);
+        q_val = InterpolationUtilities::interpolate((X[dim] - xx) / dx[dim], i_list, y_data);
+    }
+    else if (cycle == NDIM - 1)
+    {
+        std::vector<int> i_list;
+        std::vector<double> y_data;
+        for (std::vector<CellIndex<NDIM> >::const_iterator cit = idx_list.begin(); cit != idx_list.end(); ++cit)
+        {
+            const CellIndex<NDIM>& cidx = *cit;
+            i_list.push_back(cidx(dim));
+            y_data.push_back(q_data(cidx, cycle + 1));
+        }
+        const double* dx = pgeom->getDx();
+        const double* xlow = pgeom->getXLower();
+        const CellIndex<NDIM>& idxl = pbox.lower();
+        double xx = xlow[dim] + dx[dim] * (idx_list[0](dim) - idxl(dim) + 0.5);
+        q_val = InterpolationUtilities::interpolate((X[dim] - xx) / dx[dim], i_list, y_data);
+    }
+    else
+    {
+        // Shouldn't get here.
+        TBOX_ERROR("Invalid dimension.\n");
+    }
+    return q_val;
+}
 } // namespace IBTK
--- examples/complex_fluids/ex1/InterpolationUtilities.h
+++ examples/complex_fluids/ex1/InterpolationUtilities.h
@@ -41,15 +41,36 @@ namespace IBTK
 class InterpolationUtilities
 {
 public:
-    static double interpolate(const std::vector<double>& Q,
+    // Use polynomial interpolation
+    static double interpolate(const std::vector<double>& X,
                               const int data_idx,
                               SAMRAI::tbox::Pointer<SAMRAI::pdat::CellVariable<NDIM, double> > Q_var,
                               SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > patch_hierarchy,
                               const std::vector<SAMRAI::solv::RobinBcCoefStrategy<NDIM>*>& bc_coefs,
                               const double data_time,
                               const int depth = 0);
+    // Use least squares interpolation
+    static double interpolateL2(const std::vector<double>& X,
+                                const int data_idx,
+                                SAMRAI::tbox::Pointer<SAMRAI::pdat::CellVariable<NDIM, double> > Q_var,
+                                SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > patch_hierarchy,
+                                const std::vector<SAMRAI::solv::RobinBcCoefStrategy<NDIM>*>& bc_coefs,
+                                const double data_time,
+                                const int depth = 0);
     static double weightFcn(const std::vector<double>&, const std::vector<double>&);
 
+private:
+    static double interpolate(const double& x, const std::vector<int>& xi, const std::vector<double>& yi);
+
+    static double interpolateInBoxes(const CellIndex<NDIM>& idx,
+                                     const std::vector<double>& X,
+                                     SAMRAI::pdat::CellData<NDIM, int>& r_data,
+                                     SAMRAI::pdat::CellData<NDIM, double>& q_data,
+                                     Pointer<CartesianPatchGeometry<NDIM> > pgeom,
+                                     const Box<NDIM>& pbox,
+                                     int dim,
+                                     int cycle,
+                                     std::vector<int>& completed_dims);
 };
 
 } // namespace IBTK
